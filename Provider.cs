8303f602f179664b17d9400b4a4ab5888f75f8b9
MEGHA SHARMA <64322456+megha938@users.noreply.github.com>
2022-08-15 15:18:11 +0530

    Add files via upload

---
+++ Provider.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+
+namespace CodeDemoCore
+{
+    public class ProviderAss
+    {
+        public static void Main(string[] args)
+        {
+            var objClient = new Client();
+            objClient.Query();
+            Console.Read();
+        }
+    }
+
+    public static class UtilityExtensions
+    {
+        public static string Dump(this PatientInfo patInfo)
+        {
+            return ($"{patInfo.MRN},{patInfo.Name},{patInfo.Age},{patInfo.ContactNumber},{patInfo.Email}");
+        }
+    }
+
+    public class PatientInfo
+    {
+        //constructor
+        public PatientInfo(string mrn)
+        {
+            this.mrn = mrn;
+        }
+        //Backing Field - Memory
+        private readonly string mrn;
+        //Public Property
+        public string MRN { get { return this.mrn; } }
+
+
+        //Auto implemented Properties
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string ContactNumber { get; set; }
+
+        //Public Field
+        public string Email;
+
+    }
+
+
+    public class PatientCsvProvider
+    {
+        public string FilePath { get; set; }
+        public List<PatientInfo> GetAllPatients()
+        {
+
+            var csvData = File.ReadAllLines(FilePath);
+
+            var query = new List<PatientInfo>(from line in csvData
+                                              let data = line.Split(',')
+                                              select new PatientInfo(data[0])
+                                              {
+                                                  Name = data[1],
+                                                  Age = Convert.ToInt32(data[2]),
+                                                  ContactNumber = data[3],
+                                                  Email = data[4]
+                                              });
+            return query;
+        }
+    }
+
+    public class Client
+    {
+        public void Query()
+        {
+            var provider = new PatientCsvProvider();
+            provider.FilePath = @"D:\CSharp\Patient.csv";
+            IEnumerable<PatientInfo> patients = provider.GetAllPatients();
+            var result = patients.Where(p => p.Age > 30);
+            foreach (var patient in result)
+            {
+                Console.WriteLine(patient.Dump()); 
+            }
+        }
+    }
+}
---
+++ VitalManagementAssignment.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VitalManagementAssignment
+{
+    public class VitalManagementApp
+    {
+        public event Action Alarm;
+        public int HeartReading { get; set; }
+
+        public void HeartRateVitalSensor()
+        {
+            Random num = new Random();
+            HeartReading = num.Next(50, 160);
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"Heart Rate : {HeartReading}");
+            if (HeartReading < 70 || HeartReading > 120)
+            {
+                this.NotifyObserver();
+            }
+        }
+
+        private void NotifyObserver()
+        {
+            if (Alarm != null)
+            {
+                Alarm.Invoke(); 
+            }
+
+        }
+
+    }
+
+    public class Vibrator
+    {
+        public void Vibrate()
+        {
+            Console.WriteLine("Vibrate");
+        }
+    }
+
+    public class Beeper
+    {
+        public void BeepSound()
+        {
+            Console.WriteLine("Beep");
+        }
+    }
+
+
+    class vital
+    {
+        static void Main(string[] args)
+        {
+            VitalManagementApp _app = new VitalManagementApp();
+
+            Vibrator _vibrator = new Vibrator();
+            Beeper _beep = new Beeper();
+           
+
+            _app.Alarm += _vibrator.Vibrate;
+            _app.Alarm += _beep.BeepSound;
+
+            while (true)
+            {
+                _app.HeartRateVitalSensor();
+                _app.Display();
+                System.Threading.Thread.Sleep(3000);
+            }
+        }
+    }
+}
